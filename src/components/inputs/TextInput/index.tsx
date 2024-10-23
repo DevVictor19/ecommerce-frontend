f61e3017aa241fb6d889426bb795a2e1f53a9ff4
DevVictor19 <antoniovictor12@live.com>
2024-10-23 19:00:44 -0300

    feat: implement TextInput

--- src/components/inputs/TextInput/index.tsx
+++ src/components/inputs/TextInput/index.tsx
@@ -1,21 +1,45 @@
+import { cn } from '@/utils/cn';
+
 type TextInputProps = {
   label: string;
   name: string;
+  placeholder?: string;
   type?: 'text' | 'password';
   disabled?: boolean;
+  errorMessage?: string;
+  error?: boolean;
 };
 
 export default function TextInput({
   label,
   name,
+  placeholder,
   type,
   disabled,
+  errorMessage,
+  error,
 }: TextInputProps) {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input name={name} id={name} type={type} disabled={disabled} />
-      <p></p>
+    <div className="flex flex-col">
+      <label className="text-secondary-dark" htmlFor={name}>
+        {label}
+      </label>
+      <input
+        className={cn(
+          'h-12 w-full rounded-md border border-slate-200 bg-slate-100 px-4 text-secondary-dark outline-secondary-default',
+          {
+            'border-red-500 focus:outline-none': error,
+          },
+        )}
+        name={name}
+        id={name}
+        type={type}
+        disabled={disabled}
+        placeholder={placeholder}
+      />
+      {errorMessage && (
+        <p className="mt-1 text-xs font-medium text-red-500">{errorMessage}</p>
+      )}
     </div>
   );
 }
