19af4243ccbbefd2d60db5145f079175cabf27c0
DevVictor19 <antoniovictor12@live.com>
2024-10-24 22:07:10 -0300

    feat: add fowardRef input

--- src/components/inputs/TextInput/index.tsx
+++ src/components/inputs/TextInput/index.tsx
@@ -1,45 +1,45 @@
+/* eslint-disable react/display-name */
+import { forwardRef, InputHTMLAttributes } from 'react';
+
 import { cn } from '@/utils/cn';
 
 type TextInputProps = {
+  className?: string;
+  id: string;
   label: string;
-  name: string;
-  placeholder?: string;
-  type?: 'text' | 'password';
-  disabled?: boolean;
   errorMessage?: string;
   error?: boolean;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export default function TextInput({
-  label,
-  name,
-  placeholder,
-  type,
-  disabled,
-  errorMessage,
-  error,
-}: TextInputProps) {
-  return (
-    <div className="flex flex-col">
-      <label className="text-secondary-dark" htmlFor={name}>
-        {label}
-      </label>
-      <input
-        className={cn(
-          'h-12 w-full rounded-md border border-slate-200 bg-slate-100 px-4 text-secondary-dark outline-secondary-default',
-          {
-            'border-red-500 focus:outline-none': error,
-          },
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  (
+    { className, id, label, errorMessage, error, ...rest }: TextInputProps,
+    ref,
+  ) => {
+    return (
+      <div className={cn('flex flex-col', className)}>
+        <label className="text-secondary-dark" htmlFor={id}>
+          {label}
+        </label>
+        <input
+          className={cn(
+            'h-12 w-full rounded-md border border-slate-200 bg-slate-100 px-4 text-secondary-dark outline-secondary-default',
+            {
+              'border-red-500 focus:outline-none': error,
+            },
+          )}
+          {...rest}
+          id={id}
+          ref={ref}
+        />
+        {errorMessage && (
+          <p className="mt-1 text-xs font-medium text-red-500">
+            {errorMessage}
+          </p>
         )}
-        name={name}
-        id={name}
-        type={type}
-        disabled={disabled}
-        placeholder={placeholder}
-      />
-      {errorMessage && (
-        <p className="mt-1 text-xs font-medium text-red-500">{errorMessage}</p>
-      )}
-    </div>
-  );
-}
+      </div>
+    );
+  },
+);
+
+export default TextInput;
