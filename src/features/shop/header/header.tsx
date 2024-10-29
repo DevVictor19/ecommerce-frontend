7f45201cf0411beb47a4a1039aecbe265d30989f
DevVictor19 <antoniovictor12@live.com>
2024-10-29 19:07:23 -0300

    feat: add home link to header

--- src/features/shop/header/header.tsx
+++ src/features/shop/header/header.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link';
 import { ShoppingBag } from 'react-feather';
+
+import { APP_ROUTE } from '@/enums/app-routes.enum';
 
 import Avatar from '../avatar/avatar';
 import CartButton from '../cart-button/cart-button';
@@ -6,12 +9,12 @@ import CartButton from '../cart-button/cart-button';
 export default function Header() {
   return (
     <header className="navbar bg-base-100 relative z-10 px-6 drop-shadow-md">
-      <div className="flex flex-1 items-center gap-3">
+      <Link className="flex flex-1 items-center gap-3" href={APP_ROUTE.SHOP}>
         <div>
           <ShoppingBag />
         </div>
         <h1 className="font-semibold">E-commerce</h1>
-      </div>
+      </Link>
       <div className="flex items-center gap-2">
         <CartButton />
         <Avatar />
