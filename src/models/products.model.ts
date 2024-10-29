a8c3d2c35f2b1a24534b4e311d83bb457bea6d76
DevVictor19 <antoniovictor12@live.com>
2024-10-29 19:05:08 -0300

    refactor: rename model file

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -2,11 +2,21 @@ import { useQuery } from '@tanstack/react-query';
 
 import { API_ENDPOINT } from '@/enums/api-endpoints.enum';
 import { FindAllProductsRequest } from '@/services/products/contracts';
-import { findAllProducts } from '@/services/products/products.service';
+import {
+  findAllProducts,
+  findProductById,
+} from '@/services/products/products.service';
 
 export function useFindAllProductsQuery(params?: FindAllProductsRequest) {
   return useQuery({
     queryFn: () => findAllProducts(params),
     queryKey: [`${API_ENDPOINT.PRODUCTS}`, { params }],
+  });
+}
+
+export function useFindProductById(id: string) {
+  return useQuery({
+    queryFn: () => findProductById({ id }),
+    queryKey: [`${API_ENDPOINT.PRODUCTS}/${id}`],
   });
 }
