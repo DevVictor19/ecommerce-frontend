aef1b479ca83a7fc73d786619defcd0a477fda85
DevVictor19 <antoniovictor12@live.com>
2024-10-31 22:52:59 -0300

    feat: clear cache on logout

--- src/features/shop/avatar/avatar.model.ts
+++ src/features/shop/avatar/avatar.model.ts
@@ -1,12 +1,15 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 import { deleteCookie } from '@/lib/client-cookies';
 
 export function useAvatarModel() {
+  const queryClient = useQueryClient();
   const { refresh } = useRouter();
 
   const logout = () => {
     deleteCookie('authToken');
+    queryClient.clear();
     refresh();
   };
 
