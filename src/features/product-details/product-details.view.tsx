28464450c68ed5e445e9fe602cf764508d71af5f
DevVictor19 <antoniovictor12@live.com>
2024-11-03 15:40:08 -0300

    refactor: use loading screen

--- src/features/product-details/product-details.view.tsx
+++ src/features/product-details/product-details.view.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import AlertInfo from '@/components/feedback/AlertInfo';
+import LoadingScreen from '@/components/feedback/LoadingScreen';
 
 import { useProductDetailsViewModel } from './product-details.view-model';
 import ProductDetailsContent from './product-details-content/product-details-content';
@@ -17,11 +18,7 @@ export default function ProductDetailsView() {
   }
 
   if (!data || isLoading) {
-    return (
-      <div className="flex min-h-[calc(100vh-64px)] items-center justify-center">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
+    return <LoadingScreen />;
   }
 
   return <ProductDetailsContent product={data} />;
