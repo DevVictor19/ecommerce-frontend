ac2e97f9880a97b7608ab0466418f22c3c28b71f
DevVictor19 <antoniovictor12@live.com>
2024-11-03 14:46:08 -0300

    refactor: position notification top left

--- src/features/product-details/product-details-content/product-details-content.view-model.ts
+++ src/features/product-details/product-details-content/product-details-content.view-model.ts
@@ -16,18 +16,24 @@ export function useProductDetailsContentViewModel(product: Product) {
     try {
       await mutateAsync({ productId: product.id, quantity });
 
-      toast.success(`${quantity} product(s) have been added to the cart`);
+      toast.success(`${quantity} product(s) have been added to the cart`, {
+        position: 'top-left',
+      });
     } catch (err) {
       if (isAxiosError(err)) {
         const { message } = err.response?.data as AxiosError<{
           message: string;
         }>;
 
-        toast.error(message);
+        toast.error(message, {
+          position: 'top-left',
+        });
         return;
       }
 
-      toast.error('Unable to add products to cart');
+      toast.error('Unable to add products to cart', {
+        position: 'top-left',
+      });
     } finally {
       handleReset();
     }
