bbc59470b19c0e26660b5d7394da599d9bcacc26
DevVictor19 <antoniovictor12@live.com>
2024-11-05 20:30:21 -0300

    fix: fix pagination buttons

--- src/components/navigation/Pagination/index.tsx
+++ src/components/navigation/Pagination/index.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-
 import { cn } from '@/utils/cn';
+import { usePagination } from './pagination.hook';
 
 type PaginationProps = {
   currentPage: number;
@@ -10,33 +9,18 @@ type PaginationProps = {
   onChangePage: (page: number) => void;
 };
 
-const maxButtons = 7;
-
 export default function Pagination({
   currentPage,
   totalPages,
   onChangePage,
 }: PaginationProps) {
-  const [firstPage, setFirstPage] = useState(currentPage);
-
-  const start = 0; // index
-  const end = totalPages - 1; // index
-
-  const handleChangePrev = () => {
-    const prevPage = currentPage - 1;
-
-    if (prevPage < end) return;
-
-    onChangePage(prevPage);
-  };
-
-  const handleChangeNext = () => {
-    const nextPage = currentPage + 1;
-
-    if (nextPage > end) return;
-
-    onChangePage(nextPage);
-  };
+  const {
+    firstPage,
+    handleChangeComplex,
+    handleChangeNext,
+    handleChangePrev,
+    maxButtons,
+  } = usePagination({ currentPage, onChangePage, totalPages });
 
   if (totalPages <= maxButtons) {
     return (
@@ -44,7 +28,7 @@ export default function Pagination({
         <button
           className="join-item btn"
           type="button"
-          onChange={handleChangePrev}
+          onClick={handleChangePrev}
         >
           «
         </button>
@@ -59,24 +43,6 @@ export default function Pagination({
       </div>
     );
   }
-
-  const handleChangeComplex = (page: number) => {
-    let newFirstPage = page - 3; // index
-
-    if (newFirstPage < start) {
-      newFirstPage = start;
-    }
-
-    const lastPage = newFirstPage + maxButtons - 1;
-
-    if (lastPage > end) {
-      const overflow = lastPage - end;
-      newFirstPage -= overflow;
-    }
-
-    setFirstPage(newFirstPage);
-    onChangePage(page);
-  };
 
   let count = firstPage;
 
