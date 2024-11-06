414f5945a354062e821b43439fb0d449d2f8ce9b
DevVictor19 <antoniovictor12@live.com>
2024-11-06 14:28:07 -0300

    feat: add cancel my order model

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -3,6 +3,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { API_ENDPOINT } from '@/enums/api-endpoints.enum';
 import { FindAllMyOrdersRequest } from '@/services/orders/contracts';
 import {
+  cancelMyOrder,
   createMyOrder,
   findAllMyOrders,
 } from '@/services/orders/orders.service';
@@ -24,5 +25,17 @@ export function useFindAllMyOrders(params?: FindAllMyOrdersRequest) {
   return useQuery({
     queryFn: () => findAllMyOrders(params),
     queryKey: [API_ENDPOINT.CLIENT_ORDERS, { params }],
+  });
+}
+
+export function useCancelMyOrder() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: cancelMyOrder,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [API_ENDPOINT.CLIENT_ORDERS] });
+      queryClient.invalidateQueries({ queryKey: [API_ENDPOINT.PRODUCTS] });
+    },
   });
 }
