abf78934e8756324eb5e7fa5910f0f63b63e7ba5
DevVictor19 <antoniovictor12@live.com>
2024-11-09 18:56:34 -0300

    feat: add payments model

---
+++ src/models/payments.model.ts
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { API_ENDPOINT } from '@/enums/api-endpoints.enum';
+import { payWithCreditCard } from '@/services/payments/payments.service';
+
+export function usePayWithCreditCard() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: payWithCreditCard,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [API_ENDPOINT.CLIENT_ORDERS] });
+    },
+  });
+}
