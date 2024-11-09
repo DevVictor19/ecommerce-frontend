364a919c605bed5c422f7672b767ce327b26cab2
DevVictor19 <antoniovictor12@live.com>
2024-11-09 18:51:11 -0300

    feat: add credit card payments endpoint

--- src/enums/api-endpoints.enum.ts
+++ src/enums/api-endpoints.enum.ts
@@ -2,5 +2,7 @@ export enum API_ENDPOINT {
   AUTH = '/auth',
   PRODUCTS = '/products',
   CLIENT_CARTS = '/carts/my-cart',
+  ORDERS = '/orders',
   CLIENT_ORDERS = '/orders/my-orders',
+  CREDIT_CARD_PAYMENTS = '/payments/credit',
 }
