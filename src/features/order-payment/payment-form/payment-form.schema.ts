5f0ea996679cabbfb8cd8765921440dcb997f13e
DevVictor19 <antoniovictor12@live.com>
2024-11-09 19:09:04 -0300

    feat: validate dates

--- src/features/order-payment/payment-form/payment-form.schema.ts
+++ src/features/order-payment/payment-form/payment-form.schema.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
 import { removeSpecialCharacters } from '@/utils/masks';
+import { isValidMonth, isValidYear } from '@/utils/validation';
 
 const cardSchema = z.object({
   holderName: z.string().min(3).max(55).transform(removeSpecialCharacters),
   number: z.string().length(19).transform(removeSpecialCharacters),
-  expiryMonth: z.string().length(2).transform(removeSpecialCharacters),
-  expiryYear: z.string().length(4).transform(removeSpecialCharacters),
+  expiryMonth: z
+    .string()
+    .length(2)
+    .transform(removeSpecialCharacters)
+    .refine(isValidMonth),
+  expiryYear: z
+    .string()
+    .length(4)
+    .transform(removeSpecialCharacters)
+    .refine((str) => isValidYear(str, 2026, 2050)),
   ccv: z.string().length(3).transform(removeSpecialCharacters),
 });
 
