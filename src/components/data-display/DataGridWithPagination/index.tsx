311bde32c3d54bf623c94e595af1765dc548654d
DevVictor19 <antoniovictor12@live.com>
2024-11-19 09:54:42 -0300

    feat: implement DataGridWithPagination

---
+++ src/components/data-display/DataGridWithPagination/index.tsx
@@ -0,0 +1,60 @@
+import { ReactNode } from 'react';
+
+import AlertInfo from '@/components/feedback/AlertInfo';
+
+import DataGrid from '../DataGrid';
+import DataGridPagination from '../DataGridPagination';
+
+type DataGridWithPaginationProps<T extends Record<string, string | number>> = {
+  className?: string;
+  isError: boolean;
+  isLoading: boolean;
+  data: T[] | undefined;
+  columns: string[];
+  page?: {
+    size: number;
+    number: number;
+    totalElements: number;
+    totalPages: number;
+  };
+  render: (row: T, index: number) => ReactNode;
+  onChangePage: (page: number) => void;
+  onChangeSize: (size: number) => void;
+};
+
+export default function DataGridWithPagination<
+  T extends Record<string, string | number>,
+>({
+  className,
+  isError,
+  isLoading,
+  data,
+  columns,
+  page,
+  render,
+  onChangePage,
+  onChangeSize,
+}: DataGridWithPaginationProps<T>) {
+  if (isError) {
+    return (
+      <AlertInfo message="Something went wrong. Please, try again later..." />
+    );
+  }
+
+  return (
+    <div className={className}>
+      <DataGrid
+        className="h-96"
+        isLoading={isLoading}
+        data={data}
+        columns={columns}
+        render={render}
+      />
+      <DataGridPagination
+        page={page}
+        onChangePage={onChangePage}
+        onChangeSize={onChangeSize}
+      />
+    </div>
+  );
+}
