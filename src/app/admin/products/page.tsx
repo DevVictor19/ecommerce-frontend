4450376c7a566e6af061883f05552c32ad52bfea
DevVictor19 <antoniovictor12@live.com>
2024-11-19 09:55:15 -0300

    feat: implement products grid

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,3 +1,5 @@
+import ProductsView from '@/features/admin/products/products.view';
+
 export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return <ProductsView />;
 }
---
+++ src/features/admin/products/products-grid/products-grid.tsx
@@ -0,0 +1,86 @@
+import DataGridWithPagination from '@/components/data-display/DataGridWithPagination';
+import { Product } from '@/services/products/contracts';
+import { formatPrice } from '@/utils/format-price';
+
+type ProductsGridProps = {
+  isError: boolean;
+  isLoading: boolean;
+  products: Product[] | undefined;
+  page?: {
+    size: number;
+    number: number;
+    totalElements: number;
+    totalPages: number;
+  };
+  onChangePage: (page: number) => void;
+  onChangeSize: (size: number) => void;
+};
+
+const columns = [
+  'Product',
+  'Name',
+  'Price',
+  'Stock Qnt',
+  'Photo URL',
+  'Description',
+  'Created At',
+];
+
+export default function ProductsGrid({
+  isError,
+  isLoading,
+  products,
+  page,
+  onChangePage,
+  onChangeSize,
+}: ProductsGridProps) {
+  return (
+    <DataGridWithPagination
+      isError={isError}
+      isLoading={isLoading}
+      columns={columns}
+      data={products}
+      onChangePage={onChangePage}
+      onChangeSize={onChangeSize}
+      render={(row, index) => (
+        <ProductGridRow key={row.id} index={index} product={row} />
+      )}
+      page={page}
+    />
+  );
+}
+
+type ProductGridRowProps = {
+  product: Product;
+  index: number;
+};
+
+function ProductGridRow({ product, index }: ProductGridRowProps) {
+  return (
+    <tr className="hover">
+      <td>{index + 1}</td>
+      <td>{product.name}</td>
+      <td>
+        <span className="badge badge-success">
+          ${formatPrice(product.price)}
+        </span>
+      </td>
+      <td>
+        <span className="badge badge-error">{product.stockQuantity}</span>
+      </td>
+      <td>
+        <a
+          className="text-primary whitespace-nowrap underline"
+          href={product.photoUrl}
+          target="_blank"
+        >
+          Product Photo
+        </a>
+      </td>
+      <td>{product.description}</td>
+      <td className="whitespace-nowrap">
+        {new Date(product.createdAt).toLocaleDateString('en')}
+      </td>
+    </tr>
+  );
+}
