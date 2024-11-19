c12566a9ebd7db96d068e05bce410c38f323ca7a
DevVictor19 <antoniovictor12@live.com>
2024-11-19 09:53:50 -0300

    refactor: use products pagination hook

--- src/features/shop/shop/shop.view-model.ts
+++ src/features/shop/shop/shop.view-model.ts
@@ -1,53 +1,16 @@
-import { useState } from 'react';
-
+import { useProductsPagination } from '@/hooks/products-pagination.hook';
 import { useFindAllProducts } from '@/models/products.model';
-import { FindAllProductsParams } from '@/services/products/contracts';
-
-import { FilterKind } from './filter-option/filter-option';
 
 export function useShopViewModel() {
-  const [currentFilter, setCurrentFilter] =
-    useState<FilterKind>('new-products');
-
-  const [params, setParams] = useState<Partial<FindAllProductsParams>>({
-    page: 0,
-    sortBy: 'createdAt',
-    sort: 'DESC',
-  });
+  const {
+    currentFilter,
+    handleChangeFilter,
+    handleChangePage,
+    handleChangeSearch,
+    params,
+  } = useProductsPagination();
 
   const { data, isError, isLoading } = useFindAllProducts(params);
-
-  const handleChangePage = (newPage: number) => {
-    setParams((prev) => ({ ...prev, page: newPage }));
-  };
-
-  const handleChangeSearch = (value: string) => {
-    setParams((prev) => ({ ...prev, name: value }));
-  };
-
-  const handleChangeFilter = (filterKind: FilterKind) => {
-    switch (filterKind) {
-      case 'new-products':
-        setParams((prev) => ({ ...prev, sortBy: 'createdAt', sort: 'DESC' }));
-        setCurrentFilter('new-products');
-        break;
-
-      case 'alphabetical':
-        setParams((prev) => ({ ...prev, sortBy: 'name', sort: 'ASC' }));
-        setCurrentFilter('alphabetical');
-        break;
-
-      case 'cheapest':
-        setParams((prev) => ({ ...prev, sortBy: 'price', sort: 'ASC' }));
-        setCurrentFilter('cheapest');
-        break;
-
-      case 'most-expansive':
-        setParams((prev) => ({ ...prev, sortBy: 'price', sort: 'DESC' }));
-        setCurrentFilter('most-expansive');
-        break;
-    }
-  };
 
   return {
     currentFilter,
