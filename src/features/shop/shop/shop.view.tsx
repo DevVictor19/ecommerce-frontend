b6e2249210074ba959ea2a793aa3bdb7adc9cead
DevVictor19 <antoniovictor12@live.com>
2024-11-19 09:53:33 -0300

    refactor: use global filters button and search bar

--- src/features/shop/shop/shop.view.tsx
+++ src/features/shop/shop/shop.view.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import Pagination from '@/components/navigation/Pagination';
+import { ProductPaginationFilter } from '@/hooks/products-pagination.hook';
 
-import FiltersButton from './filters-button/filters-button';
+import FiltersButton, {
+  FilterButtonOption,
+} from '../../../components/ui/FiltersButton';
+import SearchBar from '../../../components/ui/SearchBar';
 import ProductsGrid from './products-grid/products-grid';
-import SearchBar from './search-bar/search-bar';
 import { useShopViewModel } from './shop.view-model';
+
+const options: FilterButtonOption<ProductPaginationFilter>[] = [
+  { value: 'new-products', label: 'New Products' },
+  { value: 'alphabetical', label: 'A-Z' },
+  { value: 'cheapest', label: 'Cheapest' },
+  { value: 'most-expansive', label: 'Most Expensive' },
+];
 
 export default function ShopView() {
   const {
@@ -23,8 +33,10 @@ export default function ShopView() {
       <div className="mb-10 flex items-center justify-center gap-4 px-5">
         <SearchBar onSearch={handleChangeSearch} />
         <FiltersButton
+          className="dropdown-end"
           currentFilter={currentFilter}
           onSelect={handleChangeFilter}
+          options={options}
         />
       </div>
       <ProductsGrid data={data} isError={isError} isLoading={isLoading} />
