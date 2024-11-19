3a363f829855ac07a6f0046cb82a2b1399eab7f1
DevVictor19 <antoniovictor12@live.com>
2024-11-19 11:48:38 -0300

    feat: add format to locale string fn

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,3 +1,7 @@
 export function formatDateString(iso: string) {
   return new Date(iso).toLocaleString('en-US');
 }
+
+export function formatLocaleDateString(iso: string) {
+  return new Date(iso).toLocaleDateString('en');
+}
