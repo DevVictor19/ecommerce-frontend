9f9b627c32ce6a1911431ecb3ca3866712179457
DevVictor19 <antoniovictor12@live.com>
2024-11-19 21:39:55 -0300

    feat: trim string values

--- src/features/admin/products/product-form/product-form.schema.ts
+++ src/features/admin/products/product-form/product-form.schema.ts
@@ -4,9 +4,9 @@ export const productFormSchema = z.object({
   price: z
     .string()
     .refine((num) => parseFloat(num) > 0, 'Price should be a positive value'),
-  name: z.string().min(4).max(100),
-  description: z.string().min(4).max(100),
-  photoUrl: z.string().url(),
+  name: z.string().min(4).max(100).trim(),
+  description: z.string().min(4).max(100).trim(),
+  photoUrl: z.string().url().trim(),
   stockQuantity: z
     .string()
     .refine((num) => Number(num) > 0, 'Stock Qnt should be a positive value'),
