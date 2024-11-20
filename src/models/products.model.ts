773b9da54a174da6e32287789ac5eda077c19fa9
DevVictor19 <antoniovictor12@live.com>
2024-11-20 10:59:04 -0300

    feat: add edit product model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -5,6 +5,7 @@ import { FindAllProductsRequest } from '@/services/products/contracts';
 import {
   createProduct,
   deleteProduct,
+  editProduct,
   findAllProducts,
   findProductById,
 } from '@/services/products/products.service';
@@ -16,10 +17,11 @@ export function useFindAllProducts(params?: FindAllProductsRequest) {
   });
 }
 
-export function useFindProductById(productId: string) {
+export function useFindProductById(productId: string, enabled?: boolean) {
   return useQuery({
     queryFn: () => findProductById(productId),
     queryKey: [API_ENDPOINT.PRODUCTS, productId],
+    enabled,
   });
 }
 
@@ -37,6 +39,16 @@ export function useDeleteProduct() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [API_ENDPOINT.PRODUCTS] });
+    },
+  });
+}
+
+export function useEditProduct() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: editProduct,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [API_ENDPOINT.PRODUCTS] });
     },
